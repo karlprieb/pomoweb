60dd7ee3bc25705eaef951aa754d0240b8a64bc7
Karl Prieb <karl@prieb.me>
2016-11-09 18:06:20 -0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <header>
+      <logo></logo>
+      <h1>PomoWeb</h1>
+    </header>
+    <main>
+      <counter></counter>
+    </main>
+  </div>
+</template>
+
+<script>
+import 'normalize.css'
+import Logo from './components/Logo'
+import Counter from './components/Counter'
+
+export default {
+  name: 'app',
+  components: {
+    Logo,
+    Counter
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Josefin+Sans:100,300');
+#app {
+  font-family: 'Josefin Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  align-items: center;
+  background: rgb(241,255,226); /* Old browsers */
+  background: linear-gradient(to bottom,  rgba(241,255,226,1) 0%,rgba(255,173,182,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+}
+h1 {
+  font-weight: 100;
+  color: rgba(0,0,0,0.5);
+  margin: 30px 0 0;
+  font-size: 3rem;
+}
+header {
+  margin-top: 20px;
+  text-align: center;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,78 @@
+<template>
+<div class="counter">
+  <section class="timer">
+    <span class="minutes">{{minutes}}</span>
+    <span class="middle">:</span>
+    <span class="seconds">{{seconds}}</span>
+  </section>
+  <section class="buttons">
+    <button class="start button">
+      <span>Start</span>
+    </button>
+  </section>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      pomoTimer: 1500,
+      shortTimer: 300,
+      longTimer: 600,
+      minutes: 0,
+      seconds: 0
+    }
+  },
+  methods: {
+    setCounter (counter) {
+      this.minutes = Math.floor(counter / 60)
+      this.seconds = counter - this.minutes * 60
+    },
+    start (counter) {
+      setTimeout(() => {
+        counter -= 1;
+      }, 1000)
+    }
+
+  },
+  mounted () {
+    this.setCounter(this.pomoTimer)
+  }
+}
+</script>
+
+<style scoped>
+.counter {
+  margin-top: 30px;
+}
+.timer {
+  color: #fff;
+  font-size: 5rem;
+  text-shadow: 0 2px 4px rgba(0, 0, 0, .3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.middle {
+  position: relative;
+  top: -.6rem;
+}
+.buttons {
+  display: flex;
+  justify-content: center;
+}
+.button {
+  border: 1px solid #fff;
+  border-radius: 50px;
+  background: rgba(255, 255, 255, .2);
+  width: 80px;
+  height: 80px;
+}
+.button span {
+  text-transform: uppercase;
+  font-size: 1rem;
+  color: rgba(0, 0, 0, .8);
+  font-weight: 100;
+}
+</style>
